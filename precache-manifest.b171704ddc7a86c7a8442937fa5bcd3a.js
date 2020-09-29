599b837f65a2d08ae8877c5823493ff90a753112
DTDUONG98 <dotienduongptit@gmail.com>
2020-09-29 16:38:46 +0700

    Updates

---
+++ precache-manifest.b171704ddc7a86c7a8442937fa5bcd3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597e933929592df6150fb9b572c0f036",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "bd2af8ccfdefc53869cc",
+    "url": "/react-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "265c78b85ad833007867",
+    "url": "/react-deploy/static/js/2.80c48c05.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-deploy/static/js/2.80c48c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2af8ccfdefc53869cc",
+    "url": "/react-deploy/static/js/main.415a2bb4.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  }
+]);
